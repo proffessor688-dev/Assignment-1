5f692d0065aa6d75ee23c70b1ab2e60ab34b9d88
proffessor688-dev <proffessor688@gmail.com>
2026-04-07 23:59:21 +0530

    added database connection

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import connectDB from "./utils/db.js";
 
 const app = express();
 const PORT = 5000;
+
+connectDB();
+
 app.get("/", (req, res) => {
   res.status(200).send({message:'Server Running Succesfully'});
 });
