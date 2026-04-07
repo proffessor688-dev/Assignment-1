d70332d6b74c25fa31a6903429c58fed8ce7def7
proffessor688-dev <proffessor688@gmail.com>
2026-04-07 23:23:14 +0530

    Intial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+app.get("/", (req, res) => {
+  res.status(200).send({message:'All set boss'});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Runing at ${PORT}`);
+});
